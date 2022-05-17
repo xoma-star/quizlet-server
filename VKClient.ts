3e5beed1a5f6243c049f2ab2fcb5eb7c2f758fd8
xoma_star <sasha.sirotyin09@gmail.com>
2022-05-17 21:18:48 +0500

    17.05.2022

--- VKClient.ts
+++ VKClient.ts
@@ -1,6 +1,7 @@
 import {Socket} from 'socket.io'
 import {firestore} from "./firebase";
 import {doc, getDoc, updateDoc} from 'firebase/firestore'
+import sleep from "./functions/sleep";
 
 export interface VKClient{
     vkid: string,
@@ -16,7 +17,8 @@ interface queues {
     oneVSall: {[k: string]: string[]},
     oneVSone: {[k: string]: string[]},
     teamVSteam: {[k: string]: string[]},
-    teamVSall: {[k: string]: string[]}
+    teamVSall: {[k: string]: string[]},
+    one: {[k: string]: string[]}
 }
 
 interface playerInRoom{
@@ -30,7 +32,8 @@ interface room{
     id: string,
     players: playerInRoom[],
     questions: any[],
-    theme: string
+    theme: string,
+    mode: string
 }
 
 export class VKClients{
@@ -39,7 +42,8 @@ export class VKClients{
         oneVSall: {},
         oneVSone: {},
         teamVSteam: {},
-        teamVSall: {}
+        teamVSall: {},
+        one: {}
     }
     rooms: room[] = []
 
@@ -85,9 +89,10 @@ export class VKClients{
             //@ts-ignore
             Object.keys(this.queues[v]).forEach(x => {
                 //@ts-ignore
-                this.queues[v][x] = this.queues[v][x].filter((x: string) => x === id)
+                this.queues[v][x] = this.queues[v][x].filter((x: string) => x !== id)
             })
         })
+        console.log(this.queues)
     }
 
     updateRoomData(room: string, data: any){
@@ -99,59 +104,107 @@ export class VKClients{
         }
     }
 
+    broadcastRoom(room: string, message: string, data?: any){
+        this.rooms.find(x => x.id === room)?.players.forEach(x => this.sendToClient(x.id, message, data))
+    }
+
+    async playRoom(room: room){
+        for (const x of room.questions) {
+            this.broadcastRoom(room.id, 'newQuestion', {question: x})
+            await sleep(x.time * 1000)
+        }
+    }
+
     async addUserDataToRoom(data: {id: string, name: string, ava: string, room: string}, callback: () => void){
         let r = this.rooms.findIndex(x => x.id === data.room)
         let a: playerInRoom = {id: data.id, name: data.name, ava: data.ava, score: 0}
         let b = this.rooms[r].players.findIndex(x => x.id === data.id)
         this.rooms[r].players[b] = a
         callback()
-        this.rooms[r].players.forEach(x => this.sendToClient(x.id, 'updatedRoomData', this.rooms[r]))
+        this.broadcastRoom(this.rooms[r].id, 'updatedRoomData', this.rooms[r])
+        if(!this.rooms[r].players.every((x) => typeof x.ava === 'undefined')) {
+            this.broadcastRoom(this.rooms[r].id, 'roomReady')
+            setTimeout(() => this.playRoom(this.rooms[r]), 5000)
+        }
     }
 
-    createRoom(players: string[], theme: string){
+    createRoom(players: string[], theme: string, mode: string){
         const roomID = Date.now().toString() + players[0]
         this.rooms.push({
             id: roomID,
             players: players.map(v => {return {id: v}}),
-            questions: [],
-            theme: theme
+            questions: [{
+                type: 'select',
+                text: 'Гнидой был Ленин. А кто еще был пидором?',
+                time: 10,
+                answers: [
+                    {text: 'Пыня', right: true},
+                    {text: 'Сталин', right: false},
+                    {text: 'Пушник', right: false},
+                    {text: 'GAME_SEARCH', right: false}
+                ]
+            },
+                {
+                    type: 'select',
+                    text: 'Гнидой был Ленин. А кто еще был пидором?',
+                    time: 10,
+                    answers: [
+                        {text: 'Пыня', right: true},
+                        {text: 'Сталин', right: false},
+                        {text: 'Пушник', right: false},
+                        {text: 'GAME_SEARCH', right: false}
+                    ]
+                }],
+            theme: theme,
+            mode: mode
         })
         return roomID
     }
 
-    async insertQueue(data: {modesSelected: ('oneVSone' | 'oneVSall' | 'teamVSall' | 'teamVSteam')[], themesSelected: string[]}, callback: () => void, id: string){
+    checkQueue(){
+
+    }
+
+    async insertQueue(data: {modesSelected: ('oneVSone' | 'oneVSall' | 'teamVSall' | 'teamVSteam' | 'one')[], themesSelected: string[]}, callback: () => void, id: string){
         const vkid = this.getVKID(id)
-        data.modesSelected.forEach(v => {
-            data.themesSelected.forEach(x => {
-                if(typeof this.queues[v][x] === 'undefined') this.queues[v][x] = []
-                this.queues[v][x].push(vkid)
-                switch (v){
-                    case "oneVSall":
-                        if(this.queues[v][x].length === 5){
-                            this.queues[v][x].forEach(z => this.sendToClient(z, 'foundGame'))
-                            return
-                        }
-                        break
-                    case "teamVSall":
-                        break
-                    case "oneVSone":
-                        if(this.queues[v][x].length === 2){
-                            const room = this.createRoom(this.queues[v][x], x)
+        try{
+            data.modesSelected.forEach(v => {
+                data.themesSelected.forEach(x => {
+                    if(typeof this.queues[v][x] === 'undefined') this.queues[v][x] = []
+                    this.queues[v][x].push(vkid)
+                    switch (v){
+                        case "one":
+                            const room = this.createRoom(this.queues[v][x], x, v)
                             this.queues[v][x].forEach(z => {
-                                console.log('before: ', this.queues)
                                 setTimeout(() => this.sendToClient(z, 'foundGame', {room: room}), 1000)
                                 this.removeFromQueue(z)
-                                console.log('after: ', this.queues)
                             })
+                            throw {}
+                        case "oneVSall":
+                            if(this.queues[v][x].length === 5){
+                                this.queues[v][x].forEach(z => this.sendToClient(z, 'foundGame'))
+                                return
+                            }
                             break
+                        case "teamVSall":
                             break
-                        }
-                        break
-                    case "teamVSteam":
-                        break
-                }
+                        case "oneVSone":
+                            if(this.queues[v][x].length === 2){
+                                const room = this.createRoom(this.queues[v][x], x, v)
+                                this.queues[v][x].forEach(z => {
+                                    setTimeout(() => this.sendToClient(z, 'foundGame', {room: room}), 1000)
+                                    this.removeFromQueue(z)
+                                })
+                                throw {}
+                            }
+                            break
+                        case "teamVSteam":
+                            break
+                    }
+                })
             })
-        })
+        }
+        catch {}
         await updateDoc(doc(firestore, 'users', this.getVKID(id)), {preferredThemes: data.themesSelected})
         callback()
     }
